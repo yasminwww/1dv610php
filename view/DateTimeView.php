fbf009db037f48c42956a0175e2e123dea7d3ef1
Yasmin <ym222bs@student.lnu.se>
2018-09-08 18:59:08 +0200

    clock done

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,8 +5,16 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = date_default_timezone_set('Europe/Stockholm');
 
-		return '<p>' . $timeString . '</p>';
+		$info = getdate();
+		$date = $info['mday'];
+		$month = $info['mon'];
+		$year = $info['year'];
+		$hour = $info['hours'];
+		$min = $info['minutes'];
+
+		return '<p>' . $current_date = "$date/$month/$year | $hour:$min" . '</p>';
+
 	}
 }
