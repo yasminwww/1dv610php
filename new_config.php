8ab7de8942ca242ac6447e8921b5ce70d815b937
Yasmin <ym222bs@student.lnu.se>
2018-09-18 19:39:01 +0200

    hardcoded admin

--- new_config.php
+++ new_config.php
@@ -6,7 +6,7 @@ IMPORTANT ->    Constants can be accessed regardless of scope
                 Constant values can only be strings and numbers
 */
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'mysqldb');
